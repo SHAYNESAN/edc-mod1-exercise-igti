ad7ca9bba061559945f61eadc154de0f1834d917
SHAYNESAN <sandro.dutra1@hotmail.com>
2022-03-17 12:26:36 -0300

    d

---
+++ infrastructure/s3.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "dl" {
+    bucket = "datalake-sandro-igti-edc-tf"
+    acl = "private"
+
+    tags = {
+
+        IES = "IGTI"
+        CURSO = "EDC"
+    }
+
+    server_side_encryption_configuration {
+
+        rule {
+
+            apply_server_side_encryption_by_default 
+        {
+
+            see_algorithm = "AES256"
+        }
+        }
+    }
+  
+}
